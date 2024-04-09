23dd43856bd7f765bbf63482d9df2796d8958ea4
Malmi Withanage <umnew1@gmail.com>
2024-04-09 19:25:21 +0530

    done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Intro duction</p>
+        <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
 
