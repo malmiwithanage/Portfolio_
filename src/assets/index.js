7697d84f9e6e1043d7f291578e5ce9a08373027f
malmiwithanage <umnew1@gmail.com>
2025-01-11 17:38:31 +0530

    commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,4 @@
-import logo from "./m2.png";
-import malmi from "./ml2.png";
+import logo from "./logo.png";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -7,9 +6,6 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-
-
-
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
@@ -27,18 +23,29 @@ import threejs from "./tech/threejs.svg";
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
+import xdev from "./company/xdev.png" 
 
-import carrent from "./3.png";
-import jobit from "./2.png";
-import tripguide from "./tripguide.png";
+import carrent from "./carrent.png";
+import p2 from "./p2.png";
 import p3 from "./p3.png";
-import p4 from "./p4.png";
-import p5 from "./p5.png";
-import p6 from "./p6.png";
+
+import p1 from "./p1.png"
+import p4 from "./p4.png"
+import p5 from "./p5.png"
+import p6 from "./p6.png"
+import p7 from "./p7.png"
+
+import s1 from "./s1.png"
+import s2 from "./s2.png"
+import s3 from "./s3.png"
+
 
 export {
+  s1,
+  s2,
+  s3,
+  p1,
   logo,
   backend,
   creator,
@@ -62,13 +69,13 @@ export {
   threejs,
   meta,
   shopify,
-  starbucks,
+  xdev,
   tesla,
   carrent,
-  jobit,
-  tripguide,
+  p2,
   p3,
   p4,
   p5,
-  p6
+  p6,
+  p7
 };
